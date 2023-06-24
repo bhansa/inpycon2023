82ccad4a013bccd6fa92776535fdaaa7ef401a49
bhansa <bsaraswat45@gmail.com>
2023-06-24 15:33:08 +0530

    init animation works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import { AppProps } from "next/app";
+import {useEffect} from "react";
+import AOS from "aos";
 import "../styles/css/style.css";
 
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // Animation duration
+      once: true, // Only animate elements once
+    });
+  }, []);
   return (
     <>
       <Head>
